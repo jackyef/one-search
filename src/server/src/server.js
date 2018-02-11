cee601ac6b5945d19d10e86e07be830dc60200fe
Jacky Efendi <jacky.efendi1@gmail.com>
2018-02-11 16:38:19 +0700

    Added shopee, but not working yet

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 import path from 'path';
 import https from 'https';
 import request from 'request';
+import rj from 'request-json';
 import url from 'url';
 import querystring from 'querystring';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 let app = express();
 
 app.use('/', express.static(path.join(__dirname, '../../client/build')));
+
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`One-search server listening on port ${process.env.PORT}!`)
@@ -76,5 +80,111 @@ app.get('/bl', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.send(body);
     })
+  })
+})
+
+const shopeeDomain = 'shopee.co.id';
+app.get('/shopee', (req, res) => {
+  const keyword = req.query.keyword;
+    
+  const payload1 = {
+    by: 'pop',
+    order: 'desc',
+    newest: 0,
+    limit: 20,
+    keyword: keyword
+  }
+
+  request({
+    headers: {
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+    },
+    method: 'GET',
+    url: `https://${shopeeDomain}/api/v1/search_items/?`,
+    qs: payload1,
+  }, (err, response, body) => {
+    // console.log("first body", body);
+    let itemsPayload;
+    let cookies;
+    try { 
+      itemsPayload = JSON.parse(body).items;
+      cookies = response.headers['set-cookie'];
+    } catch (err) {
+      console.log('first error', err);
+      res.send(err);
+      return;
+    }
+    const payload = {
+      item_shop_ids: itemsPayload 
+    };
+
+    let cookieString = cookies.reduce((acc, v) => acc + v);
+    cookieString += '; csrftoken=GXkIGmRcmE8iagMLteNX731j9n306IVb';
+    // console.log("cookie", cookieString);
+    // cookieString += '; SPC=GXkIGmRcmE8iagMLteNX731j9n306IVb';
+    request({
+      method: 'POST',
+      url: `https://${shopeeDomain}/api/v1/items/`,
+      headers: {
+        'accept': 'application/json',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        'user-agent': 'Mozilla/5.0/ (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+        'dnt': 1,
+        'if-none-match-': '55b03-11f5830fd3d639dde11608814c04c920',
+        'origin': 'https://shopee.co.id',
+        'referer': `https://shopee.co.id/search/?keyword=${keyword}`,
+        'cookie': cookieString,
+        'x-csrftoken': 'GXkIGmRcmE8iagMLteNX731j9n306IVb',
+        'x-requested-with': 'XMLHttpRequest',
+        'x-api-source': 'pc',
+      },
+      body: JSON.stringify(payload),
+      // json: true,
+    }, (err, response, body) => {
+      if(err) {
+        res.status(500),
+        res.send(err.message);
+        return;
+      }
+      res.setHeader('Content-Type', 'application/json');
+      // res.setHeader('Content-Type', 'text/html');
+      // res.send(body);
+      res.send(response);
+    })
+  })
+})
+
+app.get('/shopeetest', (req, res) => {
+  request({
+    method: 'POST',
+    url: `https://${shopeeDomain}/api/v1/items/`,
+    headers: {
+      // 'accept': 'application/json',
+      'accept-encoding': 'gzip, deflate, br',
+      // 'content-type': 'application/json',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+      'dnt': 1,
+      'if-none-match-': '55b03-11f5830fd3d639dde11608814c04c920',
+      'origin': 'https://shopee.co.id',
+      'referer': `https://shopee.co.id/search/?keyword=sepeda`,
+      'cookie': 'SPC_F=GY9kiTYnLb7l2Cr7XyEq1AXhyx4tOQ34; REC_T_ID=9712d9ee-0350-11e8-9f47-d0946603d5c9; cto_lwid=b478c6cc-e010-423f-bf11-62b17c8a92bb; __BWfp=c1517050578783x42dacbd5e; csrftoken=GXkIGmRcmE8iagMLteNX731j9n306IVb; SPC_IA=-1; SPC_EC=-; SPC_T_ID="PM5745qBiscM41hPGEawYzfWEQC+QsebJmlNzntvmqlK+zX+GuT6nfSqC6APMXe+pSUnuCspPkAhaj41D5TiKHpFdsDoBIFCK2zTmX+hmw0="; SPC_U=-; SPC_T_IV="amfPuRJ45xQsBJGeV/vlig=="; bannerShown=true; _ga=GA1.3.1460046940.1517050530; _gid=GA1.3.1984642348.1517997019; _gac_UA-61904553-8=1.1517060420.CjwKCAiA47DTBRAUEiwA4luU2bu7p4NeXA9fzfGKKKT8Ge5ASW9M4JtS6TmTVd95sGkTDiBOe7ivLBoCD8MQAvD_BwE; SPC_SC_TK=; UYOMAPJWEMDGJ=; SPC_SC_UD=; _gat=1; SPC_SI=w5twtzyexxtlb1rwo4gvsroakuo6ya1g',
+      'x-csrftoken': 'GXkIGmRcmE8iagMLteNX731j9n306IVb',
+      'x-requested-with': 'XMLHttpRequest',
+      'x-api-source': 'pc',
+    },
+    body: JSON.stringify(require('./payload.json')),
+    // json: true,
+  }, (err, response, body) => {
+    if(err) {
+      res.status(500),
+      res.send(err.message);
+      return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.send(body);
+    res.send(response);
   })
 })
---
+++ src/server/src/test.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var payload = require('./payload.json');
+
+request({
+  method: 'POST',
+  url: `https://shopee.co.id/api/v1/items/`,
+  headers: {
+    'accept':'application/json',
+    'accept-encoding':'gzip, deflate, br',
+    'accept-language':'en-US,en;q=0.9',
+    'content-length': JSON.stringify(payload).length,
+    'content-type':'application/json',
+    'cookie':'SPC_F=GY9kiTYnLb7l2Cr7XyEq1AXhyx4tOQ34; REC_T_ID=9712d9ee-0350-11e8-9f47-d0946603d5c9; cto_lwid=b478c6cc-e010-423f-bf11-62b17c8a92bb; __BWfp=c1517050578783x42dacbd5e; csrftoken=GXkIGmRcmE8iagMLteNX731j9n306IVb; SPC_IA=-1; SPC_EC=-; SPC_T_ID="PM5745qBiscM41hPGEawYzfWEQC+QsebJmlNzntvmqlK+zX+GuT6nfSqC6APMXe+pSUnuCspPkAhaj41D5TiKHpFdsDoBIFCK2zTmX+hmw0="; SPC_U=-; SPC_T_IV="amfPuRJ45xQsBJGeV/vlig=="; bannerShown=true; SPC_SC_TK=; UYOMAPJWEMDGJ=; SPC_SC_UD=; _ga=GA1.3.1460046940.1517050530; _gid=GA1.3.1773016707.1518245803; _gac_UA-61904553-8=1.1517060420.CjwKCAiA47DTBRAUEiwA4luU2bu7p4NeXA9fzfGKKKT8Ge5ASW9M4JtS6TmTVd95sGkTDiBOe7ivLBoCD8MQAvD_BwE; _gat=1; SPC_SI=rdf9h8sqqjedg5qfjbsmt4iqedse99y0',
+    'dnt':1,
+    'if-none-match-':'55b03-0920ff6099f854b52361426a590e3e71',
+    'origin':'https://shopee.co.id',
+    'referer':'https://shopee.co.id/search/?keyword=sepeda',
+    'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+    'x-api-source':'pc',
+    'x-csrftoken':'GXkIGmRcmE8iagMLteNX731j9n306IVb',
+    'x-requested-with':'XMLHttpRequest',
+  },
+  body: JSON.stringify(payload),
+  // json: true,
+}, (err, response, body) => {
+  if(err) {
+    console.log("ERROR", err);
+    return;
+  }
+  // res.setHeader('Content-Type', 'text/html');
+  // res.send(body);
+  console.log("BODY", body);
+})
